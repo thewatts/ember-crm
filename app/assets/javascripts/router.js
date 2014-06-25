4641e6167bfa2c916c2105c15a506c7d66298be3
Nathaniel Watts <reg@nathanielwatts.com>
2014-06-24 19:44:16 -0500

    add leads

---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,11 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+App.Router.reopen({
+  location: 'auto',
+  rootURL: '/'
+});
+
+App.Router.map(function() {
+  // this.resource('posts');
+  this.route('about');
+});
